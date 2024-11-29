ed101ec524525790b238482c031363be139b6137
nestossss <netobernardino2018@gmail.com>
2024-11-28 23:32:53 -0300

    Frontend: AdminRoute

--- frontend/src/components/layoutRoutes/AdminRoute.jsx
+++ frontend/src/components/layoutRoutes/AdminRoute.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router";
+import { useSession } from "../../hooks/useSession";
 
 export default function AdminRoute(){
-   const isAdmin = false; // depois é pra testar se é admin ou não de verdade
+   const { user } = useSession(); // depois é pra testar se é admin ou não de verdade
+   const isAdmin = (user.info.tipo_usuario == 'admin'); 
 
    if(!isAdmin) return <Navigate to={'/home'}/>
    return <Outlet /> 
